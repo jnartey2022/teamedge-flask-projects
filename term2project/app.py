88e72d27c01ac9a44f148f3bcf2747ab7376427b
Joseph <jnartey2022@columbiasecondary.org>
2021-03-20 11:59:10 -0400

    Almost done with SenseHat

--- term2project/app.py
+++ term2project/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, redirect, url_for, request, render_template
+from sense_hat import SenseHat   #import the SenseHat object from the sense_hat library
+from time import sleep
 app = Flask(__name__)
 @app.route("/")
 def home_page():
@@ -16,8 +18,16 @@ def login():
    else:
       user = request.args.get('nm')
       return redirect(url_for('success',name = user))
-from sense_hat import SenseHat   #import the SenseHat object from the sense_hat library
-from time import sleep
+
+@app.route('/success/<name>')
+def success(name):
+name = request.form['nm']
+   return "
+
+
+sense = SenseHat()
+name = request.form['nm']
+sense.show_message(name)
 
 if __name__ == '__main__':
    app.run(debug = True)
